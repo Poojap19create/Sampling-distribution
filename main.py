ab7a3f660ad44123af197546c1831a733636bc64
Poojap19create <74517001+Poojap19create@users.noreply.github.com>
2022-03-29 21:06:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,36 +12,12 @@ data = df["temp"].tolist()
 
 #function to get the mean of the given data samples
 # pass the number of data points you want  as counter
-def random_set_of_mean(counter):
-    dataset = []
-    for i in range(0, counter):
-        random_index= random.randint(0,len(data)-1)
-        value = data[random_index]
-        dataset.append(value)
-    mean = statistics.mean(dataset)
-    return mean
+
 
 #function to plot the mean on the graph
-def show_fig(mean_list):
-    df = mean_list
-    mean = statistics.mean(df)
-    fig = ff.create_distplot([df], ["temp"], show_hist=False)
-    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 1], mode="lines", name="MEAN"))
-    fig.show()
 
 
 # Pass the number of time you want the mean of the data points as a parameter in range function in for loop
-def setup():
-    mean_list = []
-    for i in range(0,1000):
-        set_of_means= random_set_of_mean(100)
-        mean_list.append(set_of_means)
-    show_fig(mean_list)
-    
-    mean = statistics.mean(mean_list)
-    print("Mean of sampling distribution :-",mean )
-
-setup()
 
 
 #Code to find the mean of the raw data ("population data")
