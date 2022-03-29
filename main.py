3ee4350a7016911167512a6325b79826006ce3b8
Poojap19create <74517001+Poojap19create@users.noreply.github.com>
2022-03-29 20:58:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,27 +8,8 @@ import csv
 df = pd.read_csv("data.csv")
 data = df["temp"].tolist()
 
-# code to show the plot of raw data
-# fig = ff.create_distplot([data], ["temp"], show_hist=False)
-# fig.show()
 
 
-
-#code to find mean and std deviation of 100 data points
-# dataset = []
-# for i in range(0, 100):
-#     random_index= random.randint(0,len(data))
-#     value = data[random_index]
-#     dataset.append(value)
-# mean = statistics.mean(dataset)
-# std_deviation = statistics.stdev(dataset)
-#
-# print("Mean of sample:- ",mean)
-# print("std_deviation of sample:- ",std_deviation)
-
-
-
-##  code to find the mean of 100 data points 1000 times and plot it
 #function to get the mean of the given data samples
 # pass the number of data points you want  as counter
 def random_set_of_mean(counter):
